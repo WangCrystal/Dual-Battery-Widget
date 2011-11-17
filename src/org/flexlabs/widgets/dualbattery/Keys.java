17be0a6a65181773a2080abdabfd8d77bf3bfcb5
Artiom Chilaru <artiom@flexlabs.org>
2011-11-17 19:40:15 +0000

    [+] Analytics keys

---
+++ src/org/flexlabs/widgets/dualbattery/Keys.java
@@ -0,0 +1,13 @@
+package org.flexlabs.widgets.dualbattery;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ArtiomChi
+ * Date: 14/11/11
+ * Time: 19:19
+ */
+public class Keys {
+    public static String Flurry = "HPL4SNDDA38KUGJTDE54";
+    public static String Localytics = "c197b3b9b8b2131e85990a6-da0623f8-0ef8-11e1-9249-007bc6310ec9";
+    public static String GoogleAnalytics = "UA-12531672-2";
+}
